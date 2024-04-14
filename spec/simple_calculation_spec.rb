657842ade64c8dff3669dc1b8c14a5aa9af0cbd3
aldafari <aldafari@memphis.edu>
2024-04-14 14:36:26 -0500

    Changed test value for .add function in test file.

--- spec/simple_calculation_spec.rb
+++ spec/simple_calculation_spec.rb
@@ -5,7 +5,7 @@ require 'simple_calculation'
 RSpec.describe SimpleCalculation do
   describe ".add" do
     it "correctly adds two numbers" do
-      expect(SimpleCalculation.add(2, 3)).to eq(116)
+      expect(SimpleCalculation.add(2, 3)).to eq(5)
     end
   end
 end
