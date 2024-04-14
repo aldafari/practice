869eadcb39dd85f3924da77bf0fad3c1a61e545f
aldafari <aldafari@memphis.edu>
2024-04-14 14:26:34 -0500

    Ran first test.

--- spec/simple_calculation_spec.rb
+++ spec/simple_calculation_spec.rb
@@ -2,7 +2,7 @@
 require 'simple_calculation'
 # frozen_string_literal: true
 
-describe SimpleCalculation do
+Rspec.describe SimpleCalculation do
   describe ".add" do
     it "correctly adds two numbers" do
       expect(SimpleCalculation.add(2, 3)).to eq(5)
