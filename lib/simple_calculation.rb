7da11227efb8c57bb451458db02425d4f132baf5
aldafari <aldafari@memphis.edu>
2024-04-14 14:32:14 -0500

    Moved simple_calculation file into lib folder.

---
+++ lib/simple_calculation.rb
@@ -0,0 +1,8 @@
+# simple_calculation.rb
+class SimpleCalculation
+  def self.add(a, b)
+    a + b
+  end
+end
+
+#Inline test statements
--- spec/simple_calculation_spec.rb
+++ spec/simple_calculation_spec.rb
@@ -1,11 +1,11 @@
 # spec/simple_calculation.rb
 require 'simple_calculation'
-# frozen_string_literal: true
 
-describe SimpleCalculation do
+
+Rspec.describe SimpleCalculation do
   describe ".add" do
     it "correctly adds two numbers" do
-      expect(SimpleCalculation.add(2, 3)).to eq(6)
+      expect(SimpleCalculation.add(2, 3)).to eq(116)
     end
   end
 end
