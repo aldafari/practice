a80f23617d76953ed5f4398ce665b628dee5d250
aldafari <aldafari@memphis.edu>
2024-04-14 14:28:07 -0500

    Changed values.

--- spec/simple_calculation_spec.rb
+++ spec/simple_calculation_spec.rb
@@ -2,10 +2,10 @@
 require 'simple_calculation'
 # frozen_string_literal: true
 
-Rspec.describe SimpleCalculation do
+describe SimpleCalculation do
   describe ".add" do
     it "correctly adds two numbers" do
-      expect(SimpleCalculation.add(2, 3)).to eq(5)
+      expect(SimpleCalculation.add(2, 3)).to eq(6)
     end
   end
 end
