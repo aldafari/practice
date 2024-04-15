c7e057b8ff6a33419ed45f4791db84d243bd13fc
aldafari <aldafari@memphis.edu>
2024-04-15 12:48:02 -0500

    Did some refactoring.

--- spec/simple_calculation_spec.rb
+++ spec/simple_calculation_spec.rb
@@ -1,16 +1,18 @@
 # spec/simple_calculation.rb
 require 'simple_calculation'
 
-
 RSpec.describe SimpleCalculation do
   describe ".add" do
     it "correctly adds two numbers" do
       expect(SimpleCalculation.add(2, 3)).to eq(5)
     end
+  end
+end
 
-    describe ".add" do
-      it "correctly adds negative numbers" do
-        expect(SimpleCalculation.add(5, -2)).to eq(3)
-      end
+RSpec.describe SimpleCalculation do
+  describe ".add" do
+    it "correctly adds negative numbers" do
+      expect(SimpleCalculation.add(5, -2)).to eq(3)
+    end
   end
 end
